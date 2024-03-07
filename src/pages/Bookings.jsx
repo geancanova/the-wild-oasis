bd363682d905f1dd971daf6ead1aaa6430e054fc
geancanova <geancanovawt@gmail.com>
2024-03-07 12:06:27 -0300

    refactor: move bookings data to parent component

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,18 +1,28 @@
+import { useBookings } from "../features/bookings/useBookings";
 import BookingTable from "../features/bookings/BookingTable";
 import BookingTableOperations from "../features/bookings/BookingTableOperations";
 import AddBooking from "../features/bookings/addBooking";
+import Empty from "../ui/Empty";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-
+import Spinner from "../ui/Spinner";
 function Bookings() {
+  const { bookings, isLoading, count } = useBookings();
+
+  if (isLoading) return <Spinner />;
+
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All bookings</Heading>
-        <BookingTableOperations />
+        {bookings.length ? <BookingTableOperations /> : null}
       </Row>
 
-      <BookingTable />
+      {!bookings.length ? (
+        <Empty resourceName="bookings" />
+      ) : (
+        <BookingTable bookings={bookings} count={count} />
+      )}
       <AddBooking />
     </>
   );
